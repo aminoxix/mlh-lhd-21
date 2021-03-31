096a317a25ad302c78fdd18f2df85b38a890b6fd
ANSHUMAAN KUMAR PRASAD <75872316+amino19@users.noreply.github.com>
2021-03-31 23:57:06 +0530

    Update Program.cs

--- Health-App/Program.cs
+++ Health-App/Program.cs
@@ -8,10 +8,10 @@ namespace health_app
         {
             Console.WriteLine("Have you cuddled Developer Student Community in the past week?");
             if(Console.ReadLine().ToLower()[0] == 'y'){
-                Console.WriteLine("Developer Student Community makes you healthy!");
+                Console.WriteLine("Cheers, Developer Student Community makes you healthy!");
             }
             else{
-                Console.WriteLine("Don't go with any other community, you're unhealthy!");
+                Console.WriteLine("Don't go with any other community, you're Xtreme unhealthy!");
             }
         }
     }
