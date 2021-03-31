3c4f764398db9f893c954bbad2354259d2431ab7
ANSHUMAAN KUMAR PRASAD <75872316+amino19@users.noreply.github.com>
2021-03-31 23:50:04 +0530

    Add files via upload

---
+++ Health-App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace health_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Have you cuddled a BLAHAJ in the past week?");
+            if(Console.ReadLine().ToLower()[0] == 'y'){
+                Console.WriteLine("You are being VERY healthy!");
+            }
+            else{
+                Console.WriteLine("You are being EXTREMELY unhealthy!");
+            }
+        }
+    }
+}
