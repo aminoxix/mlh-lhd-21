cd23a62baa2eb8f0d1461171e3cbd52ad72e734c
ANSHUMAAN KUMAR PRASAD <75872316+amino19@users.noreply.github.com>
2021-04-01 19:13:58 +0530

    Add files via upload

---
+++ Note Sharing App/README.md
@@ -0,0 +1,31 @@
+# Note-Sharing-App
+Using this app you can save your own app and then share it with others via e-mail id.
+
+![sharing](https://user-images.githubusercontent.com/54389203/93631794-1d709a80-f9e4-11ea-904c-70c74f2b43b2.gif)
+
+# APK file
+[Note Sharing.zip](https://github.com/agamkoradiya/Note-Sharing-App/files/5319154/Note.Sharing.zip)
+
+# How to use it
+1. Clone this repository.
+2. Open your firebase consol.
+3. Connect your project to firebase.
+
+# Key Features
+- Add a new note.
+- Edit your saved note.
+- Delete your note.
+- You can share your note with others who are using this app via E-mail address with only readable option.
+
+# Some features list which you can add in this project.
+
+- Add alert dialog box while someone clicks on sign out or delete button.
+- You can add Undo feature.
+- Check network connection feature with cool animation.
+- Add profile fragment.
+- Sort by feature.
+- Search note feature.
+- Add password or fingerprint lock in app.
+- Add Password feature for received note.
+
+# `Happy Coding`
---
+++ Note Sharing App/app/src/main/java/com/example/notesharing/ui/fragment/AllReceivedFragment.kt
@@ -0,0 +1,140 @@
+package com.example.notesharing.ui.fragment
+
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesharing.R
+import com.example.notesharing.adapter.MyReceivedNotesAdapter
+import com.example.notesharing.model.OneNoteModel
+import com.example.notesharing.viewmodel.NoteViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
+import kotlinx.android.synthetic.main.fragment_all_received.*
+
+@AndroidEntryPoint
+class AllReceivedFragment : Fragment(R.layout.fragment_all_received) {
+
+    private val viewModel by viewModels<NoteViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getAllReceivedNotes(requireContext())
+
+        val myReceivedNotesAdapter = MyReceivedNotesAdapter(
+            AllReceivedFragmentDirections,
+            this.findNavController()
+        )
+
+        allReceivedNotesRecyclerView.adapter = myReceivedNotesAdapter
+        allReceivedNotesRecyclerView.addItemDecoration(object : RecyclerView.ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect,
+                view: View,
+                parent: RecyclerView,
+                state: RecyclerView.State
+            ) {
+                outRect.bottom = 20
+            }
+        })
+        viewModel.allReceivedNotes.observe(viewLifecycleOwner, Observer {
+            myReceivedNotesAdapter.setData(it)
+        })
+
+        // SWIPE LOGIC IMPLEMENT
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val currentReceivedNote = myReceivedNotesAdapter.allReceivedNotesList[position]
+                when (direction) {
+                    ItemTouchHelper.LEFT -> {
+                        confirmDelete(currentReceivedNote)
+                    }
+                }
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+
+                RecyclerViewSwipeDecorator.Builder(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+                    .addSwipeLeftBackgroundColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.red_400
+                        )
+                    )
+                    .addSwipeLeftActionIcon(R.drawable.ic_baseline_delete_24)
+                    .create()
+                    .decorate()
+
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(allReceivedNotesRecyclerView)
+        }
+    }
+
+    private fun confirmDelete(currentReceivedNote: OneNoteModel) {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle("Confirm Delete")
+            .setMessage("Are you sure you want to delete this received note ?")
+            .setPositiveButton("Delete") { dialog, _ ->
+                viewModel.deleteAReceivedNote(requireContext(), currentReceivedNote.id, dialog)
+                Snackbar.make(requireView(), "Delete Pending...", Snackbar.LENGTH_LONG)
+                    .setAction("Undo") {
+                        viewModel.undoDeletedReceivedNote(currentReceivedNote)
+                    }
+                    .show()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.cancel()
+            }.show()
+    }
+}
